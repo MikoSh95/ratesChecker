c203c9190a0dd8fdf5bea5d002fb15a38f9aa327
Kamil Hark <kamil.hark@sealights.io>
2021-11-19 10:46:53 +0100

    coś tam

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express from "express";
+import mongoose from "mongoose";
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+const app = express();
+const port = 3000;
+
+mongoose.connect('mongodb://root:example@localhost:27017', {user: 'root', pass: 'example'});
+
+const Offer = mongoose.model('Offer', {name: String});
+
+app.get('/offers', (req, res) => {
+    Offer.find().then(offers => {
+        res.json(offers);
+    })
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`);
 })
